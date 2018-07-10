b910c08c55849c72c7e919b7e467db7e3b6e0db8
Slambeable <slambeable@gmail.com>
2018-07-10 19:26:37 +0300

    change export on default

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const giveYourName = () => {
+export default () => {
   const questionName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${questionName}!`);
 };
