a6c3535e9b35892fc543c71f0f7f303ac12ad5ab
Slambeable <slambeable@gmail.com>
2018-07-10 11:45:30 +0300

    add giveYourName func

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+import {giveYourName} from '..';
+
 console.log("Welcome to the Brain Games!");
+
+giveYourName();
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+export const giveYourName = () => {
+    const questionName = readlineSync.question('May I have your name? ');
+    console.log("Hello, " + questionName + "!");
+}
