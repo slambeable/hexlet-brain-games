edab6a147f69ad0c07350c39452aa0658ba2e656
Slambeable <slambeable@gmail.com>
2018-07-10 19:04:13 +0300

    add Eslint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import {giveYourName} from '..';
+import { giveYourName } from '..';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 
 giveYourName();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export const giveYourName = () => {
-    const questionName = readlineSync.question('May I have your name? ');
-    console.log("Hello, " + questionName + "!");
-}
+  const questionName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${questionName}!`);
+};
