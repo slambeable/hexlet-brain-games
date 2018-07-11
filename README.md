d5250c15a4ef96a70813ce28b58e6911e6719e9b
Slambeable <slambeable@gmail.com>
2018-07-11 13:36:24 +0300

    refactoring

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # project-lvl1-s304
 # nodejs-package
 [![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
-[![Test Coverage](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/test_coverage)](https://codeclimate.com/github/codeclimate/codeclimate/test_coverage)
-[![Build Status](https://travis-ci.org/slambeable/project-lvl1-s304.svg?branch=master)](https://travis-ci.org/slambeable/project-lvl1-s304)
+[![Maintainability](https://api.codeclimate.com/v1/badges/032f4935b310aa7390cd/maintainability)](https://codeclimate.com/github/slambeable/project-lvl1-s304/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/032f4935b310aa7390cd/test_coverage)](https://codeclimate.com/github/slambeable/project-lvl1-s304/test_coverage)
--- src/index.js
+++ src/index.js
@@ -9,18 +9,14 @@ export const giveYourName = () => {
 export const helloBrainEven = () => console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
 
 export const checkParity = (name) => {
-  let result = 0;
-
-  for (; result !== 3;) {
+  for (let i = 0; i != 3; i++) {
     const randomNum = Math.round(Math.random() * 100);
     console.log(`Question: ${randomNum}`);
     const answer = readlineSync.question('Your answer: ');
     if (randomNum % 2 === 0 && answer === 'yes') {
       console.log('Correct!');
-      result += 1;
     } else if (randomNum % 2 !== 0 && answer === 'no') {
       console.log('Correct!');
-      result += 1;
     } else {
       const answerCorrect = answer === 'yes' ? 'no' : 'yes';
       return `"${answer}" is wrong answer ;(. Correct answer was "${answerCorrect}".\nLet's try again, Bill!`;
