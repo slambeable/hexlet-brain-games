91b51f16433acb5ad0647e89b0d0850695d69f42
Slambeable <slambeable@gmail.com>
2018-07-14 00:56:55 +0300

    Refactoring Brain-balance

--- src/games/balance.js
+++ src/games/balance.js
@@ -3,40 +3,28 @@ import randomNum from '../utils';
 
 const description = 'Answer "yes" if number even otherwise answer "no".\n';
 
-const sumNumForQuestion = (num) => {
+const sumAllDigitInNum = (num, numLength) => {
   const numToString = String(num);
   let result = 0;
-  for (let i = 0; i !== numToString.length; i += 1) {
+  for (let i = 0; i !== numLength; i += 1) {
     result += Number(numToString[i]);
   }
   return result;
 };
 
-const stringOfTheLargestDigits = (num, numLength) => {
-  const maxDigInNum = Math.ceil(sumNumForQuestion(num) / numLength);
-  let result = '';
-  for (let i = 0; i !== numLength; i += 1) {
-    result += maxDigInNum;
-  }
-  return String(result);
-};
-
-const differenceBetweenSumsOfTwoNumbers = (num1, num2) => {
-  const sum1 = sumNumForQuestion(num1);
-  const sum2 = sumNumForQuestion(num2);
-  return sum1 >= sum2 ? sum1 - sum2 : sum2 - sum1;
-};
+const maxDigInNum = (num, numLength) => Math.ceil(sumAllDigitInNum(num, numLength) / numLength);
 
 const theBalanceNumber = (num, numLength) => {
-  const numOfTheLargDigit = stringOfTheLargestDigits(num, numLength);
-  const diffBetweenSum = differenceBetweenSumsOfTwoNumbers(num, numOfTheLargDigit);
+  const maxDigit = maxDigInNum(num, numLength);
+  const sumNum = sumAllDigitInNum(num, numLength);
+  const maxNum = maxDigit * numLength;
   let result = '';
 
   for (let i = 0; i !== numLength; i += 1) {
-    if (i < diffBetweenSum) {
-      result += numOfTheLargDigit[i] - 1;
+    if (maxNum - sumNum > i) {
+      result += maxDigit - 1;
     } else {
-      result += numOfTheLargDigit[i];
+      result += maxDigit;
     }
   }
   return result;
@@ -46,7 +34,6 @@ const gameLogic = () => {
   const numForQuestion = randomNum(9999);
   const numForQuestionLength = String(numForQuestion).length;
   const question = numForQuestion;
-
   const correctAnswer = theBalanceNumber(numForQuestion, numForQuestionLength);
   return [question, correctAnswer];
 };
